fbc3452082650edbcc2c9eca44a612b413b3905c
joonaeng <jundevcodes@gmail.com>
2025-08-06 15:21:35 +0900

    solve: swea_9490

---
+++ swea/9490/swea_9490.py
@@ -0,0 +1,41 @@
+# SWEA 9490 문제 풀이
+
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+file_path = BASE_DIR / 'input1.txt'
+
+sys.stdin = file_path.open('r', encoding='utf-8')
+
+T = int(sys.stdin.readline().strip("\n"))
+
+# T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    col, row = map(int, sys.stdin.readline().strip("\n").split())
+
+    num_list = []
+
+    for _ in range(col):
+        num_list.append(list(map(int,sys.stdin.readline().strip("\n").split())))
+
+    max_val = 0
+    for y in range(col):
+        for x in range(row):
+            cal_val = num_list[y][x]
+
+            delta_list = []
+            for i in range(1, cal_val+1):
+                delta_list.append(i)
+                delta_list.append(-i)
+
+            for delta in delta_list:
+                if 0 <= x+delta < row:
+                    cal_val += num_list[y][x+delta]
+                if 0 <= y+delta < col:
+                    cal_val += num_list[y+delta][x]
+
+            max_val = max(max_val, cal_val)
+
+    print(f"#{test_case} {max_val}")
