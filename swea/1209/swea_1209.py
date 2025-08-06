d9d8b2a22077f39a53ceaa0164bb57a4bc72c287
joonaeng <jundevcodes@gmail.com>
2025-08-06 12:31:28 +0900

    solve: swea_1209

--- swea/1209/swea_1209.py
+++ swea/1209/swea_1209.py
@@ -4,9 +4,9 @@ for test_case in range(1, T + 1):
     tc = int(input())
 
     num_list = []
-    num_len = 100
+    num_len = 100 # 배열 길이
 
-    for y in range(num_len):
+    for y in range(num_len): # 2차원 배열에 한줄씩 담기
         num_low = list(map(int, input().split()))
         num_list.append(num_low)
 
@@ -14,12 +14,12 @@ for test_case in range(1, T + 1):
     diag_reverse_sum = 0
     max_val = 0
     for y in range(num_len):
-        low_sum = sum(num_list[y])
-        col_sum = sum(num_list[i][y] for i in range(num_len))
-        diag_forword_sum += num_list[y][y]
-        diag_reverse_sum += num_list[y][num_len-1-y]
+        low_sum = sum(num_list[y]) # 행의 합
+        col_sum = sum(num_list[i][y] for i in range(num_len)) # 2중 for문을 통해 열의 합을 담는다
+        diag_forword_sum += num_list[y][y] # 대각선 정방향 전체 배열을 순회하며 총 하나의 값을 낸다.
+        diag_reverse_sum += num_list[y][num_len-1-y] # 대각선 역방향 전체 배열을 순회하며 총 하나의 값을 낸다
         
-        max_val = max(max_val, low_sum, col_sum)
-    max_val = max(max_val, diag_forword_sum, diag_reverse_sum)
+        max_val = max(max_val, low_sum, col_sum) # 현재 max_val, low_sum, col_sum 중 가장 큰 값
+    max_val = max(max_val, diag_forword_sum, diag_reverse_sum) # 최종 max_val 대각선 정,역 방향 중 가장 큰 값
 
     print(f"#{tc} {max_val}")
