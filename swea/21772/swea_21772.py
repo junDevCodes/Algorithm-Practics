97d752b08b284790032a492f12f2d3cc8f950baa
joonaeng <jundevcodes@gmail.com>
2025-08-21 09:10:35 +0900

    solve: swea_21772

--- swea/21772/swea_21772.py
+++ swea/21772/swea_21772.py
@@ -1,12 +1,14 @@
 # SWEA 21772 문제 풀이
-import sys
-from pathlib import Path
+from collections import deque
 
-BASE_DIR = Path(__file__).resolve().parent
-file_path = BASE_DIR / 'sample_input.txt'
-sys.stdin = file_path.open('r', encoding='utf-8')
-
-T = int(sys.stdin.readline().strip("\n"))
+# import sys
+# from pathlib import Path
+#
+# BASE_DIR = Path(__file__).resolve().parent
+# file_path = BASE_DIR / 'sample_input.txt'
+# sys.stdin = file_path.open('r', encoding='utf-8')
+#
+# T = int(sys.stdin.readline().strip("\n"))
 """
 [문제 설명]
 여기에 문제 요약과 요구사항을 작성하세요.
@@ -27,9 +29,14 @@ T = int(sys.stdin.readline().strip("\n"))
 [예시 출력]
 예시 출력 내용을 작성
 """
-# T = int(input())  # 표준 입력 사용 시
+T = int(input())  # 표준 입력 사용 시
 # 여러 개의 테스트 케이스를 처리합니다.
 for test_case in range(1, T + 1):
-    num = int(sys.stdin.readline().strip("\n"))
-    print(num)
-    print(f"#{test_case}")
+    # num, rotate = map(int, sys.stdin.readline().strip("\n").split())
+    num, rotate = map(int, input().split())
+
+    # num_list = deque(list(map(int, sys.stdin.readline().strip("\n").split())))
+    num_list = deque(list(map(int, input().split())))
+    for _ in range(rotate):
+        num_list.rotate(-1)
+    print(f"#{test_case} {num_list[0]}")
