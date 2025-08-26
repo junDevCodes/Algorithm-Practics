b72f5954182857be17df965925e03514b5fe59c1
joonaeng <jundevcodes@gmail.com>
2025-08-26 23:39:46 +0900

    solve: boj_14502

--- boj/14502/boj_14502.py
+++ boj/14502/boj_14502.py
@@ -1,11 +1,11 @@
 # BOJ 14502 문제 풀이
-import sys
-from pathlib import Path
-
-# 파일 입력 설정 (로컬 테스트용)
-BASE_DIR = Path(__file__).resolve().parent
-file_path = BASE_DIR / 'sample_input.txt'
-sys.stdin = file_path.open('r', encoding='utf-8')
+# import sys
+# from pathlib import Path
+#
+# # 파일 입력 설정 (로컬 테스트용)
+# BASE_DIR = Path(__file__).resolve().parent
+# file_path = BASE_DIR / 'sample_input.txt'
+# sys.stdin = file_path.open('r', encoding='utf-8')
 
 """
 [문제 설명]
@@ -48,38 +48,63 @@ sys.stdin = file_path.open('r', encoding='utf-8')
 
 import itertools
 from collections import deque
+from copy import deepcopy
+
+delta_list = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
-def bfs(map_list, node):
-    return 0
+def bfs(map_list):
+    queue = deque(virus_index_list)
+    visited = {*virus_index_list}
+    while queue:
+        current_col, current_row = queue.popleft()
+        for dt_col, dt_row in delta_list:
+            next_col, next_row = current_col + dt_col, current_row + dt_row
+            if 0 <= next_col < map_col and 0 <= next_row < map_row:
+                if map_list[next_col][next_row] != 1 and (next_col, next_row) not in visited:
+                    map_list[next_col][next_row] = 2
+                    queue.append((next_col, next_row))
+                    visited.add((next_col, next_row))
+
+    sum_safe_place = 0
+    for col_idx in range(map_col):
+        for row_idx in range(map_row):
+            if map_list[col_idx][row_idx] == 0:
+                sum_safe_place += 1
+    return sum_safe_place
 
 
-map_col, map_row = map(int, sys.stdin.readline().strip().split())
+# map_col, map_row = map(int, sys.stdin.readline().strip().split())
+map_col, map_row = map(int, input().split())
 # print(map_col, map_row)
 
-map_info = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(map_col)]
+# map_info = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(map_col)]
+map_info = [list(map(int, input().split())) for _ in range(map_col)]
 # print(map_info)
 
 empty_index_list = []
+virus_index_list = []
 for col in range(map_col):
     for row in range(map_row):
         if map_info[col][row] == 0:
             empty_index_list.append((col, row))
+        elif map_info[col][row] == 2:
+            virus_index_list.append((col, row))
 # print(empty_index_list)
 
 wall_list = list(itertools.combinations(empty_index_list, 3))
 # print(wall_list)
 
-wall = 0
+safe_place = 0
 max_safe_place = 0
 for wall in wall_list:
-    copy_map_info = map_info.copy()
-    for idx in range(3):
-        wall_col = wall[idx][0]
-        wall_row = wall[idx][1]
+    copy_map_info = deepcopy(map_info)
+
+    for wall_col, wall_row in wall:
         copy_map_info[wall_col][wall_row] = 1
 
-    visited = []
-    wall = bfs(copy_map_info, 0)
+    safe_place = bfs(copy_map_info)
 
-    max_safe_place = max(max_safe_place, wall)
+    max_safe_place = max(max_safe_place, safe_place)
+
+print(max_safe_place)
