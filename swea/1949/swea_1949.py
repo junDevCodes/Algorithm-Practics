67dcdaa5592c4549cfb4bf9b5fbade86d04ce6b2
joonaeng <jundevcodes@gmail.com>
2025-09-18 15:21:22 +0900

    solve: swea_1949

--- swea/1949/swea_1949.py
+++ swea/1949/swea_1949.py
@@ -1,68 +1,88 @@
+# SWEA 1949 문제 풀이
 # import sys
-import copy
-
+# import time
+#
+# # 로컬 테스트용 파일 입력 설정
 # sys.stdin = open('sample_input.txt', 'r', encoding='utf-8')
 
-# 전역 변수 설정
-max_length = 0
-delta = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+"""
+[문제 설명]
+가장 긴 등산로를 찾아서 그 길이를 출력하도록
+가장 높은 봉우리에서 시작
+가로 세로 방향으로 연결되어야 한다, 높이가 같거나 낮은곳, 대각선 연결 불가
+딱 한곳을 정해 최대 K만큼 지형을 깎을 수 있다
+
+[입력]
+0. TC
+1. map_size, dig_once
+2. map_info
+
+[출력]
+1. 최대 길이
+
+[알고리즘]
+dfs
+
+[복잡도]
+- 시간: O()
+- 공간: O()
+"""
+
+d_list = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
 
-def dfs(map_info, current_row, current_col, can_dig, max_dig, map_size, visited, length):
-    global max_length
-    max_length = max(max_length, length)
+def dfs(c_row, c_col, is_dig, road):
+    global max_road, map_info
 
-    visited[current_row][current_col] = True
+    visited[c_row][c_col] = True
 
-    for d_row, d_col in delta:
-        next_row, next_col = current_row + d_row, current_col + d_col
+    for d_row, d_col in d_list:
+        n_row, n_col = c_row + d_row, c_col + d_col
 
-        if 0 <= next_row < map_size and 0 <= next_col < map_size and not visited[next_row][next_col]:
+        if 0 <= n_row < map_size and 0 <= n_col < map_size and not visited[n_row][n_col]:
+            c_height = map_info[c_row][c_col]
+            n_height = map_info[n_row][n_col]
 
-            if map_info[next_row][next_col] < map_info[current_row][current_col]:
-                dfs(map_info, next_row, next_col, can_dig, max_dig, map_size, visited, length + 1)
+            if n_height < c_height:
+                dfs(n_row, n_col, is_dig, road+1)
+                max_road = max(max_road, road+1)
+            elif not is_dig and n_height-dig_once < c_height:
+                map_info[n_row][n_col] = c_height-1
+                dfs(n_row, n_col, True, road+1)
+                max_road = max(max_road, road+1)
+                map_info[n_row][n_col] = n_height
 
-            elif can_dig:
-                for dig in range(1, max_dig + 1):
-
-                    if map_info[next_row][next_col] - dig < map_info[current_row][current_col]:
-
-                        temp_map = copy.deepcopy(map_info)
-                        temp_map[next_row][next_col] -= dig
-
-                        dfs(temp_map, next_row, next_col, False, max_dig, map_size, visited, length + 1)
-
-    visited[current_row][current_col] = False
+    visited[c_row][c_col] = False
 
 
-def solve():
-    # T = int(sys.stdin.readline().strip())
-    T = int(input())
+# T = int(sys.stdin.readline().strip())
+T = int(input())
 
-    for tc in range(1, T + 1):
-        # mnt_size, max_dig = map(int, sys.stdin.readline().strip().split())
-        mnt_size, max_dig = map(int, input().split())
-        # mnt_info = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(mnt_size)]
-        mnt_info = [list(map(int, input().split())) for _ in range(mnt_size)]
+for tc in range(1, T + 1):
+    # map_size, dig_once = map(int, sys.stdin.readline().strip().split())
+    map_size, dig_once = map(int, input().split())
 
-        max_val = 0
-        for mnt in mnt_info:
-            max_val = max(max_val, max(mnt))
+    # map_info = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(map_size)]
+    map_info = [list(map(int, input().split())) for _ in range(map_size)]
+    visited = [[False] * map_size for _ in range(map_size)]
 
-        high_loc = []
-        for row in range(mnt_size):
-            for col in range(mnt_size):
-                if mnt_info[row][col] == max_val:
-                    high_loc.append((row, col))
+    start_loc = []
+    max_val = 0
+    for row in range(map_size):
 
-        global max_length
-        max_length = 0
+        if max(map_info[row]) < max_val:
+            continue
 
-        for current_row, current_col in high_loc:
-            visited = [[False] * mnt_size for _ in range(mnt_size)]
-            dfs(mnt_info, current_row, current_col, True, max_dig, mnt_size, visited, 1)
+        for col in range(map_size):
+            if max_val < map_info[row][col]:
+                max_val = map_info[row][col]
+                start_loc = [(row, col)]
+            elif max_val == map_info[row][col]:
+                start_loc.append((row, col))
+    # print(start_loc)
 
-        print(f"#{tc} {max_length}")
+    max_road = 0
+    for row, col in start_loc:
+        dfs(row, col, False, 1)
 
-
-solve()
+    print(f"#{tc} {max_road}")
