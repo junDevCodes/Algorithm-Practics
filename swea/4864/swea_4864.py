0eafbaabaada3a8b2868217ba9de7bcb5136c75b
joonaeng <jundevcodes@gmail.com>
2025-10-10 14:57:33 +0900

    solve: swea_4864

--- swea/4864/swea_4864.py
+++ swea/4864/swea_4864.py
@@ -4,9 +4,9 @@ for test_case in range(1, T + 1):
     str1 = input()
     str2 = input()
 
-    if str1 in str2: # str2 안에 str1값이 포함되는가
+    if str1 in str2:  # str2 안에 str1값이 포함되는가
         result = 1
     else:
         result = 0
-    
+
     print(f"#{test_case} {result}")
