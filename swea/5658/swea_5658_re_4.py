75f96672f7725edfd1d6f5a698fdfed099feaad4
jundevcodes <jundevcodes@gmail.com>
2026-03-03 09:41:34 +0900

    solve: 5658 Python

---
+++ swea/5658/swea_5658_re_4.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    word = input()
+
+    cut = N // 4
+    sentence = word * 2
+
+    num_list = set()
+
+    for idx in range(N):
+        num_list.add(int(sentence[idx:idx+cut], 16))
+
+    num_list = list(num_list)
+    num_list.sort(reverse = True)
+
+    print(f"#{tc} {num_list[K-1]}")
